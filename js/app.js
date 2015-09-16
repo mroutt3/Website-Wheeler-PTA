953822c52eb6a73091fdbc85f2279c05ff5d416c
Melissa Routt <mlr66@bellsouth.net>
2015-09-16 15:38:17 -0400

    changes as of 091615

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// JavaScript
+
+
+// Hide Membership Form
+
+$(document).ready (function() {
+	$("form").hide(); 
+ });
+
+
+// Show Form when Join PTA Button is Clicked
+
+
+
+
+
+
+
+// Hide Form once Submitted
+
+
+
+
+
+// Show Thanks for Joining Wheeler Elementary PTA
+
+
+
+
+
+
+//Reset to Join PTA Button
+
+
+
+
+	
